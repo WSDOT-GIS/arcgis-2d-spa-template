82e3fad92407e2429ead8193fc09be7ca553b441
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-04-06 14:04:57 -0700

    perf: :zap: Converted imports to async

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,23 @@
-import EsriMap from "@arcgis/core/Map";
-import MapView from "@arcgis/core/views/MapView";
-import Search from "@arcgis/core/widgets/Search";
-import LocatorSearchSource from "@arcgis/core/widgets/Search/LocatorSearchSource";
 import waExtent from "./WAExtent";
 
-import "./index.css";
+import("./index.css");
 
-const map = new EsriMap({
-  basemap: "topo-vector",
+import("@arcgis/core/Map").then(({ default: EsriMap }) => {
+  const map = new EsriMap({
+    basemap: "topo-vector",
+  });
+
+  import("@arcgis/core/views/MapView").then(({ default: MapView }) => {
+    const view = new MapView({
+      container: "viewDiv",
+      map,
+      extent: waExtent,
+    });
+
+    import("./setupSearch").then(({ setupSearch }) => {
+      setupSearch(view).then((search) => {
+        search.view.ui.add(search, "top-right");
+      });
+    });
+  });
 });
-
-const view = new MapView({
-  container: "viewDiv",
-  map,
-  extent: waExtent,
-});
-
-const search = new Search({
-  view,
-  // This search is configured to only search within the extent of WA.
-  // To get the default behavior, change "includeDefaultSources" to
-  // true and remove the "sources" property.
-  includeDefaultSources: false,
-  sources: [
-    new LocatorSearchSource({
-      url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
-      filter: {
-        geometry: waExtent,
-      },
-      countryCode: "US",
-    }),
-  ],
-});
-
-view.ui.add(search, "top-right");
