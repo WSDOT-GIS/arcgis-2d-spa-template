4164d5d327e9e30793ec2be8b6e23f1c94a849fa
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-04-13 12:34:48 -0700

    feat: :recycle: Added layer to map and some refactoring

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import waExtent from "./WAExtent";
 
 import("./index.css");
 
-import("@arcgis/core/Map").then(({ default: EsriMap }) => {
+async function createLayers() {
+  const MapImageLayer = (await import("@arcgis/core/layers/MapImageLayer"))
+    .default;
+  const mpLayer = new MapImageLayer({
+    portalItem: {
+      id: "03ec5ff3609f45caa849d5afa4d92e9e",
+    },
+  });
+  return [mpLayer];
+}
+
+import("@arcgis/core/Map").then(async ({ default: EsriMap }) => {
   const map = new EsriMap({
-    basemap: "topo-vector",
+    basemap: "hybrid",
+    layers: await createLayers(),
   });
 
   import("@arcgis/core/views/MapView").then(({ default: MapView }) => {
@@ -16,7 +28,16 @@ import("@arcgis/core/Map").then(({ default: EsriMap }) => {
 
     import("./widgets/setupSearch").then(({ setupSearch }) => {
       setupSearch(view).then((search) => {
-        search.view.ui.add(search, "top-right");
+        search.view.ui.add(search, {
+          index: 0,
+          position: "top-trailing",
+        });
+      });
+    });
+
+    import("./widgets/expandGroups").then(({ setupWidgets }) => {
+      setupWidgets(view, "top-trailing", {
+         group: "top-trailing"
       });
     });
   });
--- src/widgets/expandGroups.ts
+++ src/widgets/expandGroups.ts
@@ -1,7 +1,22 @@
+import type MapView from "@arcgis/core/views/MapView";
+import type SceneView from "@arcgis/core/views/SceneView";
 import type View from "@arcgis/core/views/View";
+import type BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
 import type Expand from "@arcgis/core/widgets/Expand";
+import type LayerList from "@arcgis/core/widgets/LayerList";
+import type ListItem from "@arcgis/core/widgets/LayerList/ListItem";
+import type ListItemPanel from "@arcgis/core/widgets/LayerList/ListItemPanel";
 import type Widget from "@arcgis/core/widgets/Widget";
 
+/**
+ * Valid UI position strings.
+ * Equivalent to...
+ * ```typescript
+ * `${"top" | "bottom"}-${"leading" | "trailing" | "left" | "right"}` | "manual";
+ * ```
+ * ...but defined in this way so we don't have to modify the code
+ * in the event that Esri adds additional supported values.
+ */
 export type UIPosition = Required<
   Pick<__esri.UIAddPosition, "position">
 >["position"];
@@ -9,15 +24,15 @@ export type UIPosition = Required<
 export const uiPositionRe =
   /^(?:(?:manual)|(?:(?:top)|(?:bottom))-(?:(?:left)|(?:right)|(?:leading)|(?:trailing)))$/;
 
-type ExpandProperties = ConstructorParameters<typeof Expand>[0];
+type ExpandProperties = NonNullable<ConstructorParameters<typeof Expand>[0]>;
 
 /**
  * Determines if the input string is a valid {@link UIPosition}.
  * @param input
  * @returns
  */
-export const isValidUIPosition = (input: string): input is UIPosition =>
-  uiPositionRe.test(input);
+export const isValidUIPosition = (input: unknown): input is UIPosition =>
+  typeof input === "string" && uiPositionRe.test(input);
 
 /**
  * Creates a new object and copies the properties of the input object
@@ -36,13 +51,13 @@ function copyObjectProperties<T>(options: T) {
   return currentOptions as T;
 }
 
-type ViewAddOptions = Parameters<typeof View.prototype.ui.add>[1];
+type ViewAddOptions = NonNullable<Parameters<typeof View.prototype.ui.add>[1]>;
 
 /**
  *
  * Sets up a group of {@link Expand} elements containing the input {@link Widget}.
  * @param view - view
- * @param viewAddOptions - {@link View.prototype.add}
+ * @param viewAddOptions - View add options
  * @param expandOptions - expandOptions
  * @param widgets - widgets
  * @returns The array of {@link Expand} objects that were created to contain the {@link widgets}.
@@ -51,46 +66,160 @@ type ViewAddOptions = Parameters<typeof View.prototype.ui.add>[1];
 export async function setupExpandGroup(
   view: View,
   viewAddOptions: ViewAddOptions,
-  expandOptions: ExpandProperties,
+  expandOptions?: ExpandProperties,
   ...widgets: Widget[]
-): Promise<Expand[]> {
-  // Throw error if no widgets were specified.
-  if (widgets.length < 1) {
-    throw new TypeError("No widgets were specified.");
+) {
+  console.group(setupExpandGroup.name);
+  console.debug(`${setupExpandGroup.name} constructor`, {
+    view,
+    viewAddOptions,
+    expandOptions,
+    widgets
+  })
+  try {
+    // Throw error if no widgets were specified.
+    if (widgets.length < 1) {
+      throw new TypeError("No widgets were specified.");
+    }
+
+    // Import the Expand module.
+    const Expand = (await import("@arcgis/core/widgets/Expand")).default;
+
+    // Create expand options if not already specified.
+    if (!expandOptions) {
+      console.debug("expandOptions was null or undefined. Creating new object.");
+      expandOptions = {};
+      console.debug({expandOptions})
+    }
+
+    // If view add options is just the UIPosition, convert to object
+    // with its position property set to the original string.
+    viewAddOptions = isValidUIPosition(viewAddOptions)
+      ? { position: viewAddOptions }
+      : (viewAddOptions as __esri.UIAddPosition);
+
+    // If no group has been defined for the Expand,
+    // use the position string as the group name.
+    if (!expandOptions.group) {
+      expandOptions.group = viewAddOptions.position as string;
+    }
+    if (!expandOptions.group) {
+      const message = "There was no group specified. Expands will not be grouped";
+      console.error(message);
+      throw new TypeError(
+        message
+      );
+    }
+
+    // Create an Expand for each of the widgets.
+    const expands = widgets.map((widget, index) => {
+      const currentOptions = expandOptions
+        ? copyObjectProperties(expandOptions)
+        : {};
+
+      currentOptions.content = widget;
+      console.debug(`Expand constructor options`, currentOptions)
+      const expand = new Expand(currentOptions);
+      return {
+        component: expand,
+        index: index + 1,
+        position: currentOptions.group,
+      } as __esri.UIAddComponent;
+    });
+
+    console.debug("expand objects", expands)
+
+    // Add the newly-created Expands to the view.
+    view.ui.add(expands, viewAddOptions);
+
+    return expands;
+  } finally {
+    console.groupEnd();
   }
+}
 
-  // Import the Expand module.
-  const Expand = await import("@arcgis/core/widgets/Expand").then(
-    (i) => i.default
-  );
+type LayerListItemCreateEvent = {
+  item: ListItem;
+};
 
-  // Create expand options if not already specified.
-  if (!expandOptions) {
-    expandOptions = {};
+function hasListItem(event: unknown): event is LayerListItemCreateEvent {
+  return !!event && Object.hasOwn(event, "item");
+}
+
+/**
+ * Performs further setup tasks on a layer list item, such as adding a legend.
+ * @param param0 - Layer list item creation event object,
+ * which contains an "item" ListItem property.
+ */
+const setupLayerListItems: __esri.LayerListListItemCreatedHandler = (event) => {
+  if (!hasListItem(event)) {
+    throw new TypeError(
+      `Expected event object to have an item property with a ListItem value`
+    );
   }
+  // Add a legend to the list item panel
+  event.item.panel = {
+    content: "legend",
+  } as ListItemPanel;
+};
 
-  if (!expandOptions.group && viewAddOptions) {
-    expandOptions.group =
-      typeof viewAddOptions === "string"
-        ? viewAddOptions
-        : viewAddOptions.position;
+type LayerListOptions = NonNullable<ConstructorParameters<typeof LayerList>[0]>;
+
+async function setupLayerList(
+  properties: LayerListOptions & Required<Pick<LayerListOptions, "view">>
+) {
+  const LayerList = (await import("@arcgis/core/widgets/LayerList")).default;
+
+  properties.listItemCreatedFunction = setupLayerListItems;
+  properties.visibleElements = {
+    errors: true,
+    statusIndicators: true,
+  };
+
+  const layerList = new LayerList(properties);
+  return layerList;
+}
+
+async function setupBasemapGallery(
+  ...[properties]: ConstructorParameters<typeof BasemapGallery>
+) {
+  const BasemapGallery = (await import("@arcgis/core/widgets/BasemapGallery"))
+    .default;
+
+  const gallery = new BasemapGallery(properties);
+  return gallery;
+}
+
+type ExpandGroupSetupParams = Parameters<typeof setupExpandGroup>;
+
+export async function setupWidgets(
+  view: MapView | SceneView,
+  viewAddOptions: ExpandGroupSetupParams[1],
+  expandOptions: ExpandGroupSetupParams[2]
+) {
+  console.group(setupWidgets.name);
+  console.debug(`${setupWidgets.name} constructor`, {
+    view,
+    viewAddOptions,
+    expandOptions
+  })
+  try {
+    const [gallery, layerList] = await Promise.all([
+      setupBasemapGallery({
+        view,
+        source: {
+          portal: {
+            url: "https://wsdot.maps.arcgis.com",
+          },
+        },
+      }),
+      setupLayerList({
+        view,
+      }),
+    ]);
+  
+    setupExpandGroup(view, viewAddOptions, expandOptions, gallery, layerList);
+  } finally {
+    console.groupEnd();
   }
-  if (!expandOptions.group) {
-    throw new TypeError("There was no group specified. Expands will not be grouped");
-  }
-
-  // Create an Expand for each of the widgets.
-  const expands = widgets.map((widget) => {
-    const currentOptions = expandOptions
-      ? copyObjectProperties(expandOptions)
-      : {};
-    currentOptions.content = widget;
-    return new Expand(currentOptions);
-  });
-
-
-  // Add the newly-created Expands to the view.
-  view.ui.add(expands, viewAddOptions);
-
-  return expands;
 }
