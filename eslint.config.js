bafa05dd0660275c16947b27c649cbc395fc65c9
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-09-30 12:08:33 -0700

    chore: :wrench: Updated eslint & prettier configs
    chore: :wrench: Updated eslint config to modern flat format

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,66 @@
-/* eslint-env node */
+// @ts-check
+import eslint from "@eslint/js";
+import gitignore from "eslint-config-flat-gitignore";
+import eslintConfigPrettier from "eslint-config-prettier";
+import jsdoc from "eslint-plugin-jsdoc";
+import tseslint from "typescript-eslint";
 
-/**
- * @type {import("eslint").ESLint.ConfigData}
- */
-module.exports = {
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-type-checked",
-      "prettier",
-    ],
-    ignorePatterns: ["*.cjs", "*.js", "*.d.ts", "vite.config.ts", "*.test.ts"],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      tsconfigRootDir: __dirname,
-      project: ["./tsconfig.json", "./tools/tsconfig.json"],
-    },
-    plugins: ["@typescript-eslint"],
+export default tseslint.config(
+  gitignore({
     root: true,
-    rules: {
-      "require-await": "error",
-      "no-return-await": "warn",
+  }),
+  {
+    files: ["{src,tests,tools}/**/*.{ts,tsx,mts}"],
+    ignores: ["vite.config.ts"],
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      jsdoc,
     },
-  };
-  
+    extends: [
+      gitignore(),
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      jsdoc.configs["flat/recommended-typescript"],
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "jsdoc/require-jsdoc": [
+        "warn",
+        {
+          enableFixer: false,
+          publicOnly: true,
+        },
+      ],
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          allowAny: false,
+          allowBoolean: true,
+          allowNullish: true,
+          allowNumber: true,
+          allowRegExp: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ["{src,tests,tools}/**/*.{js,jsx,mjs}"],
+    extends: [tseslint.configs.disableTypeChecked],
+    rules: {
+      // turn off other type-aware rules
+      "deprecation/deprecation": "off",
+      "@typescript-eslint/internal/no-poorly-typed-ts-props": "off",
+
+      // turn off rules that don't apply to JS code
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
+);
