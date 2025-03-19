e3f70935001d03877138cee4fbd45cd3b5ce5584
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-03-18 17:33:44 -0700

    feat: :sparkles: Add measure and home
    Add measure component
    
    fix: Add missing import for home component.

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,4 @@
-import type { ArcgisMapCustomEvent } from "@arcgis/map-components";
 import "./index.css";
-import type { ArcgisMap } from "@arcgis/map-components/components/arcgis-map";
-
-async function addLayersToMap(
-	this: ArcgisMap,
-	event: ArcgisMapCustomEvent<void>,
-) {
-	// Dynamically import the FeatureLayer class.
-	const { default: FeatureLayer } = await import(
-		"@arcgis/core/layers/FeatureLayer"
-	);
-	const cityLimitsLayer = new FeatureLayer({
-		portalItem: {
-			id: "0d96ba84b802425aa1d2b9a99e422c5d",
-		},
-	});
-	const map = this.map ?? event.target?.map;
-	map.add(cityLimitsLayer);
-}
 
 /* 
 Since some browsers targeted in the browserslist file don't yet support
@@ -25,26 +6,42 @@ top-level await, we need to wrap our code in a self-executing async function.
 */
 (async () => {
 	// Dynamically import the components we need.
-	await Promise.all([
+	const [, , , , , , , , , , , { addLayersToMap }] = await Promise.all([
 		import("@arcgis/map-components/components/arcgis-map"),
 		import("@arcgis/map-components/components/arcgis-basemap-gallery"),
+		import("@arcgis/map-components/components/arcgis-distance-measurement-2d"),
 		import("@arcgis/map-components/components/arcgis-expand"),
+		import("@arcgis/map-components/components/arcgis-home"),
 		import("@arcgis/map-components/components/arcgis-layer-list"),
 		import("@arcgis/map-components/components/arcgis-legend"),
 		import("@arcgis/map-components/components/arcgis-locate"),
 		import("@arcgis/map-components/components/arcgis-scale-bar"),
 		import("@arcgis/map-components/components/arcgis-search"),
 		import("@arcgis/map-components/components/arcgis-zoom"),
+		import("./setup-layers"),
 	]);
 
 	// Get the map element.
 	// If your app has multiple maps, you'll need to assign each one an "id" attribute.
 	const mapElement = document.body.querySelector("arcgis-map");
 
-	if (!mapElement) {
-		throw new Error("No <arcgis-map> element found in the document.");
+	if (mapElement) {
+		// Wait for the map to load before adding layers.
+		mapElement.addEventListener("arcgisViewReadyChange", addLayersToMap);
 	}
 
-	// Wait for the map to load before adding layers.
-	mapElement.addEventListener("arcgisViewReadyChange", addLayersToMap);
+	document.body
+		.querySelector("arcgis-layer-list")
+		?.addEventListener("arcgisReady", function (this) {
+			const customizeItem: __esri.ListItemCreatedHandler = ({
+				item,
+			}: { item: __esri.ListItem }) => {
+				/* __PURE__ */ console.log("layer list item created", item);
+
+				item.panel = {
+					content: "legend",
+				};
+			};
+			this.listItemCreatedFunction = customizeItem;
+		});
 })();
