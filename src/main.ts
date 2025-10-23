43a09e30c0e056a92b98532c93a3ca33b53f46f0
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-10-23 11:58:06 -0700

    fix: add "group" attribute to expands so they don't cover each other

--- src/main.ts
+++ src/main.ts
@@ -3,49 +3,49 @@ import "./index.css";
 import type { ArcgisLayerListCustomEvent } from "@arcgis/map-components";
 
 async function setupLayerList() {
-	interface LayerListItemCreatedEvent {
-		item: __esri.ListItem;
-	}
+  interface LayerListItemCreatedEvent {
+    item: __esri.ListItem;
+  }
 
-	/**
-	 * Checks if the provided event is a {@link LayerListItemCreatedEvent}.
-	 * @param event - an event object
-	 * @returns - true if the event is a {@link LayerListItemCreatedEvent}, false otherwise
-	 */
-	function hasListItem(event: unknown): event is LayerListItemCreatedEvent {
-		return !!event && Object.hasOwn(event, "item");
-	}
+  /**
+   * Checks if the provided event is a {@link LayerListItemCreatedEvent}.
+   * @param event - an event object
+   * @returns - true if the event is a {@link LayerListItemCreatedEvent}, false otherwise
+   */
+  function hasListItem(event: unknown): event is LayerListItemCreatedEvent {
+    return !!event && Object.hasOwn(event, "item");
+  }
 
-	/**
-	 * Customize the layer list item.
-	 * @param params - layer list item created event params
-	 * @param params.item - newly created layer list item
-	 */
-	const customizeItem: __esri.ListItemCreatedHandler = (ev) => {
-		if (!hasListItem(ev)) {
-			throw new Error('Event expected to have an "item" property.');
-		}
-		ev.item.panel = {
-			content: "legend",
-			flowEnabled: true,
-		};
-	};
+  /**
+   * Customize the layer list item.
+   * @param params - layer list item created event params
+   * @param params.item - newly created layer list item
+   */
+  const customizeItem: __esri.ListItemCreatedHandler = (ev) => {
+    if (!hasListItem(ev)) {
+      throw new Error('Event expected to have an "item" property.');
+    }
+    ev.item.panel = {
+      content: "legend",
+      flowEnabled: true,
+    };
+  };
 
-	/**
-	 * Called when the layer list item is created.
-	 * @param ev - event params
-	 * @param ev.item - newly created layer list item
-	 */
-	function customizeLayerListItem(
-		this: HTMLArcgisLayerListElement,
-		ev: ArcgisLayerListCustomEvent<void>,
-	) {
-		ev.target.listItemCreatedFunction = customizeItem;
-	}
+  /**
+   * Called when the layer list item is created.
+   * @param ev - event params
+   * @param ev.item - newly created layer list item
+   */
+  function customizeLayerListItem(
+    this: HTMLArcgisLayerListElement,
+    ev: ArcgisLayerListCustomEvent<void>
+  ) {
+    ev.target.listItemCreatedFunction = customizeItem;
+  }
 
-	document.body
-		.querySelector("arcgis-layer-list")
-		?.addEventListener("arcgisReady", customizeLayerListItem);
+  document.body
+    .querySelector("arcgis-layer-list")
+    ?.addEventListener("arcgisReady", customizeLayerListItem);
 }
 
 /* 
@@ -59,11 +59,11 @@ const { addLayersToMap } = await import("./setup-layers");
 
 // Wait for the map to load before adding layers.
 document.body
-	.querySelector<HTMLArcgisMapElement>("arcgis-map")
-	?.addEventListener("arcgisViewReadyChange", addLayersToMap);
+  .querySelector<HTMLArcgisMapElement>("arcgis-map")
+  ?.addEventListener("arcgisViewReadyChange", addLayersToMap);
 
 await setupLayerList();
 
 import("./setup-search").catch((error) => {
-	console.error("Error setting up search:", error);
+  console.error("Error setting up search:", error);
 });
