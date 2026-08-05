2a5df148f66d554d255be500bc80a219e645b00a
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2026-08-05 16:24:23 -0700

    feat: click on map to show mileposts

--- src/setup-map.ts
+++ src/setup-map.ts
@@ -1,5 +1,8 @@
+import type { ScreenPoint } from "@arcgis/core/core/types";
+
 const EsriMap = await $arcgis.import("@arcgis/core/Map.js");
 const SpatialReference = await $arcgis.import("@arcgis/core/geometry/SpatialReference.js");
+const FeatureLayerView = await $arcgis.import("@arcgis/core/views/layers/FeatureLayerView.js");
 const { imageryWithWsdotRoutesBasemap } = await import("./basemaps");
 
 export const arcgisMap = document.body.querySelector<HTMLArcgisMapElement>("arcgis-map");
@@ -10,10 +13,73 @@ if (!arcgisMap) {
 
 arcgisMap.spatialReference = SpatialReference.WebMercator;
 
+const milepostsLayer = await import("./layers/mileposts").then(
+	({ milepostsLayer }) => milepostsLayer,
+);
+
 const esriMap = new EsriMap({
 	basemap: imageryWithWsdotRoutesBasemap,
+	layers: [milepostsLayer],
 });
 
-esriMap.add(import("./layers/mileposts").then(({ milepostsLayer }) => milepostsLayer));
-
 arcgisMap.map = esriMap;
+
+// Setup milepost filtering event handler
+arcgisMap.addEventListener(
+	"arcgisViewClick",
+	/**
+	 * If the user clicked near mileposts, update the milepost layer view's filter to make these
+	 * mileposts visible.
+	 */
+	async (event) => {
+		const {
+			target: { layerViews },
+			detail: { mapPoint, x, y },
+		} = event;
+
+		// If the user clicked on a feature, exit.
+		// Default action (show popup) will be performed.
+		const hitTestResult = await arcgisMap.hitTest({
+			x,
+			y,
+		});
+
+		if (hitTestResult.results.length) {
+			return;
+		}
+
+		const objectIds = (await milepostsLayer.queryObjectIds({
+			geometry: mapPoint,
+			distance: 50,
+			units: "feet",
+		})) as number[];
+
+		/* __PURE__ */ console.debug("query result", objectIds);
+
+		const mpLayerView = layerViews
+			.filter((lv) => lv.layer === milepostsLayer && lv instanceof FeatureLayerView)
+			.at(0) as InstanceType<typeof FeatureLayerView> | undefined;
+
+		/* __PURE__ */ console.debug("layer view", mpLayerView);
+
+		if (!mpLayerView) {
+			return;
+		}
+
+		if (!objectIds.length) {
+			return;
+		}
+
+		if (mpLayerView.filter?.objectIds?.length) {
+			mpLayerView.filter = {
+				objectIds: [...mpLayerView.filter.objectIds, ...objectIds],
+			};
+		} else {
+			mpLayerView.filter = {
+				objectIds,
+			};
+		}
+
+		event.preventDefault();
+	},
+);
